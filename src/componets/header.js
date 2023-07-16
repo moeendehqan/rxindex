cf12bc2222405fac4f1532c193b17cc151ea3977
moeendehqan <moeen.dehqan@gmail.com>
2023-07-16 23:46:02 +0330

    //

---
+++ src/componets/header.js
@@ -0,0 +1,19 @@
+
+
+
+
+const Header = () =>{
+    return(
+        <header>
+            <div className="logo">
+                <img src={'/img/logo.svg'}></img>
+                <img src={'/img/subLogoDashboard.svg'}></img>
+            </div>
+            <button>ورود به برنامه</button>
+
+        </header>
+    )
+}
+
+
+export default Header
